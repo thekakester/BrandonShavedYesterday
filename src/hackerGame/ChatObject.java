9ba8292ef66ee1fa7485ab64c982840891389853
Bpkuchni <bpkuchni@gmail.com>
2017-01-09 22:06:30 -0600

    adding my prelim files for chatbox

---
+++ src/hackerGame/ChatObject.java
@@ -0,0 +1,9 @@
+package hackerGame;
+
+import java.util.Date;
+
+public class ChatObject {
+	public String message;
+	public Date timestamp;
+	public String user;	
+}
---
+++ www/chatbox.js
@@ -0,0 +1,18 @@
+var mrskeltal = 
+
+$(document).ready(function(){
+	$(body).append(mrskeltal);
+});
+
+
+function escapeHtml(text) {
+  var map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;'
+  };
+
+  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
