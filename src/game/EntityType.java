f5c8200c4479c9844ee3b33e461c850774cede06
thekakester <mitcheld@mtu.edu>
2017-01-19 17:12:13 -0600

    Updated entity types in java, but I need dinner now

--- src/game/EntityType.java
+++ src/game/EntityType.java
@@ -1,8 +1,25 @@
 package game;
 
 public class EntityType {
-	public static final int PLAYER = 1;
-	public static final int SIGN = 2;
-	public static final int GRAVESTONE = 3;
-	public static final int GEM = 4;
+	public static final int PLAYER 						= 1;
+	public static final int SIGN 						= 2;
+	public static final int GRAVESTONE 					= 3;
+	public static final int GEM 						= 4;
+	public static final int WALL_BACK_TOP_LEFT 			= 5;
+	public static final int WALL_BACK_TOP_RIGHT 		= 6;
+	public static final int WALL_BACK_BOT_LEFT 			= 7;
+	public static final int WALL_BACK_BOT_RIGHT 		= 8;
+	public static final int WALL_SIDE_LEFT 				= 9;
+	public static final int WALL_SIDE_RIGHT 			= 10;
+	public static final int WALL_FRONT_TOP_LEFT 		= 11;
+	public static final int WALL_FRONT_TOP_RIGHT 		= 12;
+	public static final int WALL_FRONT_BOT_LEFT 		= 13;
+	public static final int WALL_FRONT_BOT_RIGHT 		= 14;
+	public static final int WALL_NORM_TOP 				= 15;
+	public static final int WALL_NORM 					= 16;
+	public static final int WALL_PILLAR_TOP_LEFTHALF 	= 17;
+	public static final int WALL_PILLAR_TOP_RIGHTHALF 	= 18;
+	public static final int WALL_PILLAR_BOT_LEFTHALF 	= 19;
+	public static final int WALL_PILLAR_BOT_RIGHTHALF 	= 20;
+
 }
