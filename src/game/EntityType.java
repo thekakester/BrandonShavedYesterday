13ef7f6b27e4f1c245102e2b9402852649bc70ad
thekakester <mitcheld@mtu.edu>
2017-01-19 17:58:14 -0600

    Introduced static entities and auto-saving

--- src/game/EntityType.java
+++ src/game/EntityType.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.HashSet;
+
 public class EntityType {
 	public static final int PLAYER 						= 1;
 	public static final int SIGN 						= 2;
@@ -21,5 +23,36 @@ public class EntityType {
 	public static final int WALL_PILLAR_TOP_RIGHTHALF 	= 18;
 	public static final int WALL_PILLAR_BOT_LEFTHALF 	= 19;
 	public static final int WALL_PILLAR_BOT_RIGHTHALF 	= 20;
+	public static final int SPAWNER_GHOST 				= 21;
+	public static final int GHOST 						= 22;
 
+	
+	//STATIC ENTITIES
+	//These should be saved/loaded from map files
+	//These should never move or be dynamically created by the game
+	//If you need something that moves, create a spawner instead
+	public static final HashSet<Integer> STATIC_ENTITIES = new HashSet<Integer>();
+	
+	static {
+		STATIC_ENTITIES.add(SIGN);
+		STATIC_ENTITIES.add(GRAVESTONE);
+		STATIC_ENTITIES.add(GEM);
+		STATIC_ENTITIES.add(WALL_BACK_TOP_LEFT);
+		STATIC_ENTITIES.add(WALL_BACK_TOP_RIGHT);
+		STATIC_ENTITIES.add(WALL_BACK_BOT_LEFT);
+		STATIC_ENTITIES.add(WALL_BACK_BOT_RIGHT);
+		STATIC_ENTITIES.add(WALL_SIDE_LEFT);
+		STATIC_ENTITIES.add(WALL_SIDE_RIGHT);
+		STATIC_ENTITIES.add(WALL_FRONT_TOP_LEFT);
+		STATIC_ENTITIES.add(WALL_FRONT_TOP_RIGHT);
+		STATIC_ENTITIES.add(WALL_FRONT_BOT_LEFT);
+		STATIC_ENTITIES.add(WALL_FRONT_BOT_RIGHT);
+		STATIC_ENTITIES.add(WALL_NORM_TOP);
+		STATIC_ENTITIES.add(WALL_NORM);
+		STATIC_ENTITIES.add(WALL_PILLAR_TOP_LEFTHALF);
+		STATIC_ENTITIES.add(WALL_PILLAR_TOP_RIGHTHALF);
+		STATIC_ENTITIES.add(WALL_PILLAR_BOT_LEFTHALF);
+		STATIC_ENTITIES.add(WALL_PILLAR_BOT_RIGHTHALF);
+		STATIC_ENTITIES.add(SPAWNER_GHOST);
+	}
 }
