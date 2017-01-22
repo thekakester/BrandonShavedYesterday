7d9822bddfbc0aabb3d0b4dd242ae3118e5f53bc
Bpkuchni <bpkuchni@gmail.com>
2017-01-21 18:45:43 -0600

    Added scroll bar functionality. With two new fonts.

--- www/chatbox.js
+++ www/chatbox.js
@@ -1,17 +1,31 @@
 $(document).ready(function(){
 	$("body").append("<div id=\"mrskeltal\"></div>");
 	$.get("test.html",function(data){
-		$("#mrskeltal").html(data);
+		$("#mrskeltal").html(data,setTimeout(function(){formatChatBox()},100));
+		
 	});
-	reformatCss();
+	
 });
 
-
-
-
-function reformatCss(){
+function formatChatBox(){
+	var canvasHeight = $("canvas").prop("height");
+	var topHeight = canvasHeight / 10;
+	var topWidth = topHeight * 8.34;
+	var centerHeight = canvasHeight - (2 * topHeight);
+	var marginHeight = (topHeight / 6 ) * -1;
+	centerHeight = centerHeight + (-2 * marginHeight);
 	
-	console.log($("canvas").prop("height") );
-	console.log($(".scrollcontainer") );
-	$(".scrollcontainer").prop("height", $("canvas").prop("height"));
+	$("#ScrollContainer").width(topWidth);
+	
+	$("#ScrollTop").width(topWidth);
+	$("#ScrollTop").height(topHeight);
+	
+	$("#ScrollContent").css('margin',marginHeight +'px auto');
+	$("#ScrollContent").height(centerHeight);
+	$("#ScrollContent").width(topWidth);
+	
+	$("#ScrollBottom").width(topWidth);
+	$("#ScrollBottom").height(topHeight);
+	
+	$("#ScrollContainer").removeClass("nodisp");
 }
